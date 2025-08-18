3fac05dc354c549c0a5e35ecf2f30518e0757010
Sujay Kumar Suman <sujaykumar.dev@gmail.com>
2025-08-19 01:48:39 +0530

    Add ECS clients for hello services

--- environments/hcp/hello-server/outputs.tf
+++ environments/hcp/hello-server/outputs.tf
@@ -1,11 +1,7 @@
-output "ssh_command" {
-  value = "ssh -i environments/hello-server/client-key.pem ubuntu@${module.payload_consul_server.instance_public_ip}"
+output "cluster_id" {
+  value = module.hello_server.cluster_id
 }
 
-output "instance_public_ip" {
-  value = module.payload_consul_server.instance_public_ip
-}
-
-output "instance_id" {
-  value = module.payload_consul_server.instance_id
+output "service_name" {
+  value = module.hello_server.service_name
 }
---
+++ modules/hcp/consul-client-ecs/variables.tf
@@ -0,0 +1,24 @@
+variable "hcp_project_id" {}
+variable "hcp_hvn_id" {}
+variable "hcp_cidr_block" {}
+variable "datacenter" {}
+variable "payload_image" {}
+variable "payload_command" {
+  type    = list(string)
+  default = []
+}
+variable "payload_port" {
+  type    = number
+  default = 0
+}
+variable "payload_id" {}
+variable "consul_http_addr" {}
+variable "consul_http_token" {}
+variable "task_cpu" {
+  type    = number
+  default = 256
+}
+variable "task_memory" {
+  type    = number
+  default = 512
+}
