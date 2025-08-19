56f8050bac8cc525a4b25f341b94466eb70543a8
Sujay Kumar Suman <sujaykumar.dev@gmail.com>
2025-08-19 22:45:45 +0530

    Revert "refactor consul server ssh key path"
    This reverts commit c11a9166d28b40201fc1d86863388116c318504f.

--- modules/sh/consul-server-sh/main.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  ssh_key_path = "${path.module}/../../../shared/ssh/sh-dc-server-key.pem"
-}
-
-resource "null_resource" "fix_ssh_key_permissions" {
-  provisioner "local-exec" {
-    command = "chmod 400 ${local.ssh_key_path}"
-  }
-}
